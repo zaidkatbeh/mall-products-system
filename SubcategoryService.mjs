c14514afe8165b362f6058ca8e51e687ee6836d8
zaidkatbeh <72863351+zaidkatbeh@users.noreply.github.com>
2023-10-16 14:54:58 +0300

    added the seach by function

--- SubcategoryService.mjs
+++ SubcategoryService.mjs
@@ -5,4 +5,14 @@ export class SubcategoryService {
     getAll() {
         return this.subcategories;
     }
+    searchBy(column,input) {
+        let result = -1;
+         this.categories.find((category,currentIndex) => {
+            if (category[column] == input) {
+                result = {"category":category,"index":currentIndex};
+                return;
+            }
+        })
+        return result;
+    }
 }
