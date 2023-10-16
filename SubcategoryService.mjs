3520092825d095fd8a2763a132e805d52a220c6f
zaidkatbeh <72863351+zaidkatbeh@users.noreply.github.com>
2023-10-16 15:20:42 +0300

    created the add a subcategory function

--- SubcategoryService.mjs
+++ SubcategoryService.mjs
@@ -5,14 +5,39 @@ export class SubcategoryService {
     getAll() {
         return this.subcategories;
     }
-    searchBy(column,input) {
+    getLastID() {
+        let maxID = 1;
+        this.subcategories.map((subcategory) => {
+            if (subcategory.id > maxID) {
+                maxID = subcategory.id
+            }
+        });
+        return maxID;
+
+    }
+    searchBy(column, input) {
         let result = -1;
-         this.subcategories.find((subcategory,currentIndex) => {
+        this.subcategories.find((subcategory, currentIndex) => {
             if (subcategory[column] == input) {
-                result = {"subcategory":subcategory,"index":currentIndex};
+                result = { "subcategory": subcategory, "index": currentIndex };
                 return;
             }
         })
         return result;
+    }
+    add(categoryID, name) {
+        let alreadyExist = false;
+        this.subcategories.map((subcategory) => {
+            if (subcategory.name == name && subcategory.categoryID == categoryID) {
+                alreadyExist = true;
+                return;
+            }
+        });
+        if (alreadyExist == true) {
+            console.log("there is already a subcategory with the same category id and name");
+            return -1;
+        }
+        this.subcategories.push(new Subcategory(this.getLastID() + 1, categoryID, name));
+        return 1;
     }
 }
