51ea52de65f39cdee7815f52fbaf15ef708d84a6
zaidkatbeh <72863351+zaidkatbeh@users.noreply.github.com>
2023-10-16 15:43:15 +0300

    added the edit function

--- SubcategoryService.mjs
+++ SubcategoryService.mjs
@@ -52,4 +52,16 @@ export class SubcategoryService {
         this.subcategories.splice(subcategory.index, 1);
         return 1;
     }
+    edit(id,newName) {
+        if (typeof id != "number" || id < 1 || typeof newName != "string") {
+            return -1;
+        }
+        let subcategory = this.searchBy("id",id);
+        if(subcategory == -1) {
+            console.log("subcateogry not found");
+            return -1;
+        }
+        this.subcategories[subcategory.index].name = newName;
+        return 1;
+    }
 }
