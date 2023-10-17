c667010dd1c5219cfd4be0eed2697b91077789c5
zaidkatbeh <72863351+zaidkatbeh@users.noreply.github.com>
2023-10-17 11:18:06 +0300

    added the manage offers UI

--- UserInterface.mjs
+++ UserInterface.mjs
@@ -23,7 +23,7 @@ class UserInterface {
                     this.manageProducts();
                     break;
                 case "4" :
-                    console.log("edit offers");
+                    this.manageOffers();
                     break;
                 case "5" :
                     console.log("app is closed");
@@ -116,11 +116,48 @@ class UserInterface {
                     }
             })
     }
-    
+
     manageProducts() {
         console.clear();
         console.log("-".repeat(60));
         console.log("MPMS--->manage products");
+        console.log("-".repeat(60));
+        this.printOptions(" get all","search by","add new","edit","delete","go back");
+                readl.question("enter the number of the procces you want to do? ",  (answer) => {
+                    switch(answer) {
+                        case "1" :
+                            console.log("get all");
+                            break;
+                        case "2" :
+                            console.log("search by");
+                            break;
+                        case "3" :
+                            console.log("add new ");
+                            break;
+                        case "4" :
+                            console.log("edit");
+                            break;
+                        case "5" :
+                            console.log("delete");
+                            break;
+                        case "6" :
+                            this.mainUI();
+                            break;
+                        case "7" :
+                            console.log("app closed");
+                            readl.close();
+                            break;
+                        default :
+                        console.log("please enter a valid number");
+                        this.manageCategories();
+                    }
+            })
+    }
+
+    manageOffers() {
+        console.clear();
+        console.log("-".repeat(60));
+        console.log("MPMS--->manage offers");
         console.log("-".repeat(60));
         this.printOptions(" get all","search by","add new","edit","delete","go back");
                 readl.question("enter the number of the procces you want to do? ",  (answer) => {
