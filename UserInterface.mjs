6295fd66787a25a21467fd30ca44701386d93d5b
zaidkatbeh <72863351+zaidkatbeh@users.noreply.github.com>
2023-10-17 11:05:04 +0300

    added the printOptions function

--- UserInterface.mjs
+++ UserInterface.mjs
@@ -5,22 +5,16 @@ const readl = readline.createInterface({
   });
 
 class UserInterface {
-    mainUI() {
+     mainUI() {
+        console.clear();
         console.log("-".repeat(60));
         console.log("Welcome to mall products manegment system (MPMS)");
         console.log("-".repeat(60));
-        console.log(`
-1 - Manage Categories.
-2 - Manage Subcategories.       
-3 - Manage Products.
-4 - Manage Offers.
-5 - Close the App.        
-        `);
-        let userInput ;
+        this.printOptions("Manage Categories","Manage Subcategories","Manage Products","Manage Offers")
         readl.question("enter the number of the procces you want to do? ",  (answer) => {
             switch(answer) {
                 case "1" :
-                    console.log("edit category");
+                    this.manageCategories();
                     break;
                 case "2" :
                     console.log("edit subcategory");
@@ -40,7 +34,15 @@ class UserInterface {
                 this.mainUI();
             }
         });
-    }   
+    }
+
+
+    printOptions(...options) {
+        options.forEach((option,currentIndex) => {
+            console.log(`${currentIndex + 1} - ${option}.`);
+        });
+        console.log(`${options.length + 1} - close the App`);
+    }  
 }
 let userInterface = new UserInterface();
 userInterface.mainUI();
