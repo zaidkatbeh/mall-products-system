180d6bce649cb831dabba61cc9e9e5e3abdd6f2f
zaidkatbeh <72863351+zaidkatbeh@users.noreply.github.com>
2023-10-19 10:12:31 +0300

    code convintion in category service

--- CategoryService.mjs
+++ CategoryService.mjs
@@ -1,13 +1,13 @@
 import { Category } from "./Category.mjs";
 
 export class CategoryService {
-   static categories = [];
+    static categories = [];
     // this function is already made from the last branch due to the need of it 
-   static searchBy(column,input) {
+    static searchBy(column, input) {
         let result = -1;
-         this.categories.find((category,currentIndex) => {
+        this.categories.find((category, currentIndex) => {
             if (category[column] == input) {
-                result = {"category":category,"index":currentIndex};
+                result = { "category": category, "index": currentIndex };
                 return;
             }
         })
@@ -30,36 +30,36 @@ export class CategoryService {
         if (typeof name != "string") {
             return -1;
         }
-        if (this.searchBy("name",name) != -1) {
+        if (this.searchBy("name", name) != -1) {
             console.log("there is already a category with this name");
             return -1;
         }
         return this.categories.push(new Category(this.getLastID() + 1, name));
     }
     static delete(id) {
-        if(typeof id != "number" || id < 1) {
+        if (typeof id != "number" || id < 1) {
             return -1;
         }
-        let category = this.searchBy("id",id);
-        if(category == -1) {
+        let category = this.searchBy("id", id);
+        if (category == -1) {
             console.log("category not found");
             return -1;
         }
         return (this.categories.splice(category.index, 1)) ? 1 : -1
     }
-    static edit(id,newName) {
-        let categoryIndex = this.searchBy("id",id);
-        if(categoryIndex == -1) {
+    static edit(id, newName) {
+        let categoryIndex = this.searchBy("id", id);
+        if (categoryIndex == -1) {
             console.log("category not found");
             return -1;
         }
         let isNameUsed = -1;
         this.categories.map((category) => {
-            if(category.id != id && category.name == newName) {
+            if (category.id != id && category.name == newName) {
                 isNameUsed = 1;
             }
         });
-        if(isNameUsed == -1) {
+        if (isNameUsed == -1) {
             this.categories[categoryIndex.index].name = newName;
             return 1;
         } else {
@@ -72,14 +72,14 @@ export class CategoryService {
             return -1;
         }
         let categoryIndex = -1;
-        this.categories.map((category,currentIndex) => {
+        this.categories.map((category, currentIndex) => {
             if (category.id == id) {
                 categoryIndex = currentIndex;
                 return;
             }
         })
         console.log(categoryIndex);
-        if(categoryIndex == -1) {
+        if (categoryIndex == -1) {
             console.log("category not found");
             return -1;
         }
